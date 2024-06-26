0d487fa22f5655c28863d30c5501411478f47e09
baisoyayash00 <yashubaisoya61@gmail.com>
2024-06-26 20:40:45 +0530

    PractiseTest 26062024

---
+++ eclipse-workspace/practise/src/Test/Table5.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class Table5 {
+    public static void main(String[] args) { 
+for(int i=1; i<=10; i++)
+{
+    System.out.println("5x" +i+ "=" +(5*i) );
+}
+}
+}
+
+
---
+++ eclipse-workspace/practise/src/Test/Test12.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class Test12 {
+    public static void main(String[] args) {
+for(int i=1; i<=10; i++)
+{
+if(i%2==0 && i%2!=0)
+    {
+        
+        System.out.println("even & 0dd" + ("5x" +i+ "=" +(5*i)) );
+    }
+    
+}
+    }  
+}  
+
+
