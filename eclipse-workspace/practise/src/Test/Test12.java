872e374e65e022c4a0c05a79650df5facf3274e8
baisoyayash00 <yashubaisoya61@gmail.com>
2024-06-28 18:57:14 +0530

    TestClasses

--- eclipse-workspace/practise/src/Test/Test12.java
+++ eclipse-workspace/practise/src/Test/Test12.java
@@ -2,16 +2,19 @@ package Test;
 
 public class Test12 {
     public static void main(String[] args) {
-for(int i=1; i<=10; i++)
+ int table  ;
+ int num=5;
+ for(int i=1; i<=10; i++)
+ {
+    table =num*i; //5 2 10 
+if(table%2==0)
 {
-if(i%2==0 && i%2!=0)
-    {
-        
-        System.out.println("even & 0dd" + ("5x" +i+ "=" +(5*i)) );
-    }
-    
+    System.out.println("even");
 }
-    }  
+else
+{
+    System.out.println( "odd");
+}
+ }
 }  
-
-
+}
---
+++ eclipse-workspace/practise/src/Test/Test13.java
@@ -0,0 +1,29 @@
+package Test;
+
+public class Test13 {
+public static void main(String[] args) {
+    System.out.println("yashbaisoya");
+    int table;
+    int num=23;
+    for(int i=1; i<=10; i++)
+    {
+        table = num*i;
+        int count=0;
+            for(int j=1; j<=table; j++)
+            {
+if(table%j==0)
+ count ++;
+            }
+            if(count==2)
+
+            {
+System.out.println(table);
+            }
+        }
+
+            }
+
+            }
+        
+        
+
