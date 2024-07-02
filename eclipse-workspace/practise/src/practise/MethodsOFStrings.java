37fb112873571a0530380f95c6bc4d1fd6bc5f86
baisoyayash00 <yashubaisoya61@gmail.com>
2024-07-02 16:03:32 +0530

    NewFolderAddedPractiseYash

--- eclipse-workspace/practise/src/practise/MethodsOFStrings.java
+++ eclipse-workspace/practise/src/practise/MethodsOFStrings.java
@@ -1,10 +1,15 @@
 package practise;
 
+import javafx.stage.StageStyle;
+
 public class MethodsOFStrings {
     public static void main(String[] args) {
      String str =  "Yash" ;
      System.out.println("lengthofstring" + str.length());
      System.out.println("small leters " + str.toLowerCase());
+     System.out.println("big leters" + str.toUpperCase());
+     System.out.println("name" + str.trim());
+     System.out.println( str.Substring));
      System.out.println(str);
     }
 
--- eclipse-workspace/practise/src/practise/StringIterate.java
+++ eclipse-workspace/practise/src/practise/StringIterate.java
@@ -4,7 +4,7 @@ public class StringIterate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-String str1="yashbaisoya"	;
+String str="yashbaisoya"	;
 {
 	
 }
